e6aa578c828f39ae9f4cf5ed22984d51d905e093
Mauro Casciati <maurocasciati@gmail.com>
2022-08-07 22:57:19 -0300

    add: Filters in tasklist component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Task } from '../domain/task';
-import { ListGroup } from 'react-bootstrap';
+import { Col, FloatingLabel, Form, ListGroup, Row } from 'react-bootstrap';
 import { TaskComponent } from './TaskItem';
 import './TaskList.styles.css';
 import { TaskDetailsComponent } from './TaskDetails';
@@ -9,45 +9,84 @@ const mockData: Task[] = [
   {
     name: 'Terminar maquetado del TODO List',
     description: 'Seguir codificando la aplicación react para terminar el requerimiento de COR',
-    status: 'en proceso',
-    priority: 'alta',
+    status: 'done',
+    priority: 'high',
   },
   {
     name: 'Agregar pantalla para crear tarea',
     description: 'Agregar nuevo componente para crear una tarea nueva desde la UI',
-    status: 'en proceso',
-    priority: 'alta',
+    status: 'inprogress',
+    priority: 'high',
   },
   {
     name: 'Agregar Router',
     description: 'Agregar react router para cambiar entre listado, detalle de tarea y crear tarea nueva',
-    status: 'nueva',
-    priority: 'media',
+    status: 'new',
+    priority: 'medium',
   },
   {
     name: 'Agregar Redux',
     description: 'Usar redux para manejar estado global de tareas y modificarlo al crear una nueva tarea o actualizar el estado de una existente',
-    status: 'nueva',
-    priority: 'media',
+    status: 'new',
+    priority: 'medium',
   },
   {
-    name: 'Agregar calidaciones de formulario',
+    name: 'Agregar validaciones de formulario',
     description: 'Usar react hook form para validar los datos ingresados al crear una tarea',
-    status: 'nueva',
-    priority: 'media',
+    status: 'inprogress',
+    priority: 'low',
   }
 ]
 
 export const TaskListComponent = () => {
   const [taskList, setTaskList] = useState<Task[]>(mockData);
+  const [filteredTaskList, setFilteredTaskList] = useState<Task[]>();
+  const [titleFilter, setTitleFilter] = useState<string>();
+  const [priorityFilter, setPriorityFilter] = useState<string>();
+  const [statusFilter, setStatusFilter] = useState<string>();
 
   const addNewTask = (newTask: Task) => setTaskList([ newTask, ...taskList ]);
+
+  useEffect(() => {
+    filterTasks();
+  }, [taskList, statusFilter, priorityFilter, titleFilter]);
+  
+  const filterTasks = () => setFilteredTaskList(taskList.filter((task) =>
+      (!statusFilter || task.status === statusFilter) &&
+      (!priorityFilter || task.priority === priorityFilter) &&
+      (!titleFilter || task.name.toLowerCase().includes(titleFilter.toLowerCase()))
+    ));
 
   return (
     <>
       <TaskDetailsComponent create={addNewTask} />
+
+      <Row className='Filters'>
+        <FloatingLabel as={Col} md='6' controlId='formName' label='Name:'>
+          <Form.Control type='text' value={titleFilter} onChange={(e) => setTitleFilter(e.target.value)}/>
+        </FloatingLabel>
+
+        <FloatingLabel as={Col} md='3' controlId='formPriority' label='Priority:'>
+          <Form.Select value={priorityFilter} onChange={(e) => setPriorityFilter(e.target.value)}>
+            <option value={undefined}></option>
+            <option value='low'>Low</option>
+            <option value='medium'>Medium</option>
+            <option value='high'>High</option>
+          </Form.Select>
+        </FloatingLabel>
+
+        <FloatingLabel as={Col} md='3' controlId='formStatus' label='Status:'>
+        <Form.Select value={statusFilter} onChange={(e) => setStatusFilter(e.target.value)}>
+            <option value={undefined}></option>
+            <option value='new'>New</option>
+            <option value='inprogress'>In progress</option>
+            <option value='done'>Done</option>
+          </Form.Select>
+        </FloatingLabel>
+      </Row>
+
       <ListGroup className={'List'}>
-        { taskList && taskList.map((task, index) => 
+        { filteredTaskList && filteredTaskList.map((task, index) => 
           <ListGroup.Item action key={index}><TaskComponent task={task}/></ListGroup.Item>
         )}
       </ListGroup>
